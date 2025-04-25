80453220c34e6ca70bdcd0ae71e035fbff3367ac
sims610 <sims610@byu.edu>
2025-04-25 13:55:26 -0600

    Added Offline Fallback functionality

--- startup-react/src/login/unauthenticated.jsx
+++ startup-react/src/login/unauthenticated.jsx
@@ -18,19 +18,23 @@ export function Unauthenticated(props) {
   }
 
   async function loginOrCreate(endpoint) {
-    const response = await fetch(endpoint, {
-      method: 'post',
-      body: JSON.stringify({ email: userName, password: password }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    if (response?.status === 200) {
-      localStorage.setItem('userName', userName);
-      props.onLogin(userName);
-    } else {
-      const body = await response.json();
-      setDisplayError(`⚠ Error: ${body.msg}`);
+    try {
+      const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({email: userName, password: password}),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      if (response?.status === 200) {
+        localStorage.setItem('userName', userName);
+        props.onLogin(userName);
+      } else {
+        const body = await response.json();
+        setDisplayError(`⚠ Error: ${body.msg}`);
+      }
+    } catch {
+      setDisplayError(`⚠ It appears that you are currently offline. You can play Baby Namer offline, but you must be initially online to create or login to your account.`);
     }
   }
 
